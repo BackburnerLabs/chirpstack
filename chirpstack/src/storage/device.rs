5da57e070af61b8ce86417d670bf1d23e4e490f5
Scott Elgaard <scottelgaard@gmail.com>
2025-07-14 14:59:11 -0500

    reverting last change

--- chirpstack/src/storage/device.rs
+++ chirpstack/src/storage/device.rs
@@ -904,7 +904,9 @@ pub async fn get_with_class_b_c_queue_items(limit: usize) -> Result<Vec<Device>>
             })
             .bind::<diesel::sql_types::Integer, _>(limit as i32)
             .bind::<fields::sql_types::Timestamptz, _>(Utc::now())
-            .bind::<fields::sql_types::Timestamptz, _>(Utc::now())
+            .bind::<fields::sql_types::Timestamptz, _>(
+                Utc::now() + Duration::from_std(2 * conf.network.scheduler.interval).unwrap(),
+            )
             .load(c)
             .await
             .map_err(|e| Error::from_diesel(e, "".into()))
