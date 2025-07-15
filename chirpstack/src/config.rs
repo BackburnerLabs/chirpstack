df76837cc47e9dd0d6b0ceec65fbde2c21d4e05d
Scott Elgaard <scottelgaard@gmail.com>
2025-07-15 10:38:15 -0500

    changed scheduler interval to be 100 ms

--- chirpstack/src/config.rs
+++ chirpstack/src/config.rs
@@ -204,7 +204,7 @@ pub struct Scheduler {
 impl Default for Scheduler {
     fn default() -> Self {
         Scheduler {
-            interval: Duration::from_millis(1000),
+            interval: Duration::from_millis(100),
             batch_size: 100,
             class_a_lock_duration: Duration::from_secs(5),
             class_c_lock_duration: Duration::from_secs(5),
